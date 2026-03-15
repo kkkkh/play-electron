fd81c84bb5cf5c0c5567bc358f9cf275e1d988ca
kkkkh <13693256971@163.com>
2026-03-15 08:49:25 +0000

    appData

--- src/main/ipc/on/appGetPath.ts
+++ src/main/ipc/on/appGetPath.ts
@@ -3,11 +3,13 @@ import { app, ipcMain } from 'electron'
 export const appGetPathOn = (): void => {
   ipcMain.on('appGetPath', () => {
     const userDataPath = app.getPath('userData')
+    const appDataPath = app.getPath('appData')
     const tempPath = app.getPath('temp')
     const downloadsPath = app.getPath('downloads')
     const exePath = app.getPath('exe')
     console.log(userDataPath) // C:\Users\usename\AppData\Roaming\electron
-    console.log(tempPath) // C:\Users\HAOTIA~1.ZHA\AppData\Local\Temp
+    console.log(appDataPath) // C:\Users\usename\AppData\Roaming
+    console.log(tempPath) // C:\Users\AAAA~1.BBB\AppData\Local\Temp
     console.log(downloadsPath) // C:\Users\usename\Downloads\
     console.log(exePath) // 本地路径 D:\projectname\node_modules\.pnpm\electron@35.5.1\node_modules\electron\dist\electron.exe
   })
